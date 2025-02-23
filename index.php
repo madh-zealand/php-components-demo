6dc435edca3e526478a2f690ab2bfa627fc9eb06
Martin Dilling-Hansen <madh@zealand.dk>
2025-02-24 00:32:18 +0100

    Added handling of dynamic including pages

--- index.php
+++ index.php
@@ -1,11 +1,54 @@
+<?php
+
+    // Get the page value from the url GET parameter.
+    // If the key 'page' isn't set in the GET array, default to 'dashboard'.
+    $page = $_GET['page'] ?? 'dashboard';
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <title>PHP Components Demo</title>
 </head>
 <body>
-    <h1>PHP Components</h1>
+    <header>
+        <nav>
+            <a href="?page=dashboard">Dashboard</a>
+            <a href="?page=settings">Settings</a>
+            <a href="?page=about">About</a>
+        </nav>
+    </header>
+    <main>
+        <?php
+            // Check the value of the $page variable,
+            // and include the correct page file.
+            // Page files are kept in the 'pages' folder to keep it organized.
+            match ($page) {
+                'dashboard' => include 'pages/dashboard.page.php',
+                'settings' => include 'pages/settings.page.php',
+                'about' => include 'pages/about.page.php',
+                // If $page contains another value than above, show a 404 page.
+                default => include 'pages/404.page.php',
+            };
+
+            // The above code using 'match (...)', is a prettier way to write
+            // the logic below. Functionally no difference, but it looks cleaner
+            //
+            // if ($page === 'dashboard') {
+            //     include 'pages/dashboard.page.php';
+            // } elseif ($page === 'settings') {
+            //     include 'pages/settings.page.php';
+            // } elseif ($page === 'about') {
+            //     include 'pages/about.page.php';
+            // } else {
+            //     include 'pages/404.page.php';
+            // }
+        ?>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
---
+++ pages/settings.page.php
@@ -0,0 +1,8 @@
+<h1>
+    Settings
+</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet blanditiis
+    deleniti ea eveniet fugiat fugit ipsam iste iure laudantium, magni modi
+    neque nesciunt omnis recusandae reiciendis repudiandae totam, unde!
+</p>
